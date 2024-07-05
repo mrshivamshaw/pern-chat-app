d96274d7b3f68d4036d70a138cb6b63907a2dc9b
shivamshaw07 <shivamshaw9005@gmail.com>
2024-07-05 17:57:44 +0530

    ui desgin completed

---
+++ frontend/src/components/GenderCheckbox.tsx
@@ -0,0 +1,19 @@
+const GenderCheckbox = () => {
+	return (
+		<div className='flex'>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer`} htmlFor="gender">
+					<span className='label-text'>Male</span>
+					<input id="gender" type='checkbox' className='checkbox border-slate-900' />
+				</label>
+			</div>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer`} htmlFor="gender">
+					<span className='label-text'>Female</span>
+					<input id='gender' type='checkbox' className='checkbox border-slate-900' />
+				</label>
+			</div>
+		</div>
+	);
+};
+export default GenderCheckbox;
