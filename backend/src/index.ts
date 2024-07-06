3cd3cd3bb2a3dd0eb5a364f9b1710eb0bb15f95a
shivamshaw07 <shivamshaw9005@gmail.com>
2024-07-06 22:20:15 +0530

    configuration for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,13 @@ import authRoute from "./routes/auth.route";
 import messageRoute from "./routes/message.route";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
 import { app, server } from "./socket/socket";
 import path from 'path'
 dotenv.config();
 
 const port = process.env.PORT || 3001;
 const __dirname = path.resolve();
-app.use(cors(
-    {
-        origin : ['http://localhost:5173','http://localhost:3000'],
-        credentials : true,
 
-    }
-));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth',authRoute);
