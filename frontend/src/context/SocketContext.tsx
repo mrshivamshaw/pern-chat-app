ad73e13908f805383b9a3a4ad09d7b2c7ed9308c
shivamshaw07 <shivamshaw9005@gmail.com>
2024-07-07 01:35:02 +0530

    configuration for deployment

--- frontend/src/context/SocketContext.tsx
+++ frontend/src/context/SocketContext.tsx
@@ -17,7 +17,7 @@ export const useSocketContext = (): ISocketContext => {
 	return context;
 };
 
-const socketURL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const socketURL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 const SocketContextProvider = ({ children }: { children: ReactNode }) => {
 	const socketRef = useRef<Socket | null>(null);
