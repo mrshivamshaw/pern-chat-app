2c753a4fb1dc0c1267a8b2e975c41256b0680bc2
shivamshaw07 <shivamshaw9005@gmail.com>
2024-07-07 02:13:59 +0530

    deployment configuration added

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
@@ -10,14 +11,12 @@ const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 app.use(cookieParser()); // for parsing cookies
 app.use(express.json()); // for parsing application/json
+app.use(cors({
+    origin: ["https://pern-chat-app.vercel.app", "http://localhost:5173"], // Adjust the origins as necessary
+    credentials: true
+}));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-if (process.env.NODE_ENV !== "development") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-    });
-}
 server.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
@@ -14,16 +14,13 @@ const __dirname = path.resolve();
 
 app.use(cookieParser()); // for parsing cookies
 app.use(express.json()); // for parsing application/json
+app.use(cors({
+    origin: ["https://pern-chat-app.vercel.app", "http://localhost:5173"], // Adjust the origins as necessary
+    credentials: true
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if (process.env.NODE_ENV !== "development") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 server.listen(PORT, () => {
 	console.log("Server is running on port " + PORT);
