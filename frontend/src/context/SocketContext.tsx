a9e9835d4cecdb89c6c5cff3adcda061dcd4f5a9
shivamshaw07 <shivamshaw9005@gmail.com>
2024-07-09 10:48:23 +0530

    socket cors origin

--- frontend/src/context/SocketContext.tsx
+++ frontend/src/context/SocketContext.tsx
@@ -17,7 +17,7 @@ export const useSocketContext = (): ISocketContext => {
 	return context;
 };
 
-const socketURL = import.meta.env.VITE_MODE === "development" ? "http://localhost:5000" : import.meta.env.VITE_DEPOLOYED_URL;
+const socketURL = import.meta.env.VITE_MODE !== "development" ? "http://localhost:5000" : import.meta.env.VITE_DEPOLOYED_URL;
 
 const SocketContextProvider = ({ children }: { children: ReactNode }) => {
 	const socketRef = useRef<Socket | null>(null);
