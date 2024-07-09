d9ef7c8a55a8744e6b86282c6467f87a327277f8
shivamshaw07 <shivamshaw9005@gmail.com>
2024-07-09 10:55:46 +0530

    socket cors origin

--- backend/dist/src/socket/socket.js
+++ backend/dist/src/socket/socket.js
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["https://pern-chat-app.vercel.app/"],
+        origin: ["https://pern-chat-app.vercel.app"],
         methods: ["GET", "POST"],
     },
 });
